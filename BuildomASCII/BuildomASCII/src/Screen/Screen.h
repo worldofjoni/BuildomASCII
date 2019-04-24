493c9dd8df7ba2477dde32592db315028cd977c8
Jonatan Ziegler <jonizi@t-online.de>
2019-04-24 17:07:45 +0200

    Screen merged

---
+++ BuildomASCII/BuildomASCII/src/Screen/Screen.h
@@ -0,0 +1,23 @@
+#pragma once
+#ifndef SCREEN_H
+#define SCREEN_H
+
+
+class Screen
+{
+private:
+
+public:
+	static const int 
+		WITH = 100, 
+		HEIGHT = 50;
+	static const fc::Color
+		defaultBackgroundColor = LIGHT_GRAY,
+		defaultTextColor = BLUE_LIGHT;
+	virtual void printScreen() = 0;
+	virtual ~Screen() {};
+};
+
+
+
+#endif
