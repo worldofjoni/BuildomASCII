0e9e55c3c66831937efa1f92c7ec61b83c3a0884
pylirious <samuel@tipitopi.de>
2019-05-26 12:55:25 +0200

    Added Story Level Selection

---
+++ BuildomASCII/BuildomASCII/src/Screen/StorySelect/StorySelect.h
@@ -0,0 +1,35 @@
+#pragma once
+#ifndef STORY_SELECT_H
+#define STORY_SELECT_H
+
+#include "Screen/Screen.h"
+#include "BdalManager/BdalManager.h"
+
+class StorySelect : public Screen
+{
+private:
+	BdalManager fileManager;
+
+	int x = 0;
+	int gap = 2;
+	int input = 0;
+	int nameCount = 0;
+
+	Pos start = { 4, 4 };
+	Pos current = { start.x - 1, start.y };
+
+
+	void initScreen(int prevX = 0);
+
+public:
+
+	void run();
+	StorySelect();
+	~StorySelect();
+	StorySelect(const StorySelect& other) = delete;
+	StorySelect(StorySelect&& other) = delete;
+	StorySelect& operator=(const StorySelect& other) = delete;
+	StorySelect& operator=(StorySelect&& other) = delete;
+};
+
+#endif // !STORY_SELECT_H
