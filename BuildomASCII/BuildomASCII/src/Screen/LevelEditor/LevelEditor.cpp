50f735762c8323ebdf4d4ff0584c2165f6e381b5
Jonatan Ziegler <jonizi@t-online.de>
2019-06-03 17:58:06 +0200

    fixed merge mistake

--- BuildomASCII/BuildomASCII/src/Screen/LevelEditor/LevelEditor.cpp
+++ BuildomASCII/BuildomASCII/src/Screen/LevelEditor/LevelEditor.cpp
@@ -117,32 +117,3 @@ void LevelEditor::run()
 	} while (!fileManager.saveLevel(level, input, isEditing));
 	
 }
-
-bool isInt(std::string str)
-{
-	if (str.length() >= 9) return false;
-
-	for (int i = 0; i < str.length(); i++) if (str[i] < 0) return false;
-
-	if (!(str[0] == '-' || isdigit(str[0]))) return false;
-	
-	for (int i = 1; i < str.length(); i++)
-	{
-		if (!isdigit(str[i])) return false;
-	}
-	return true;
-}
-
-bool isFilename(std::string str)
-{
-	if (str.length() >10) return false;
-
-	for (int i = 0; i < str.length(); i++) if (str[i] < 0) return false;
-
-	for (int i = 0; i<str.length(); i++)
-	{
-		if (isalnum(str[i]) == 0 && str[i] != '_')
-			return false;
-	}
-	return true;
-}
